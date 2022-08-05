203d8a2d22707cf3ffe82a2da71c8b192bb7509e
Mark Liew <mliew@u82z04s03.42kl.edu.my>
2022-08-05 22:34:48 +0800

    done

--- client.c
+++ client.c
@@ -6,11 +6,17 @@
 /*   By: mliew < mliew@student.42kl.edu.my>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/18 15:59:41 by mliew             #+#    #+#             */
-/*   Updated: 2022/08/05 12:38:50 by mliew            ###   ########.fr       */
+/*   Updated: 2022/08/05 16:09:38 by mliew            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+void	message(int sig)
+{
+	(void)sig;
+	write (1, "Message Sent.", 1);
+}
 
 void	client_handler(int pid, char *msg)
 {
@@ -25,15 +31,15 @@ void	client_handler(int pid, char *msg)
 		bit_count = 8;
 		while (--bit_count >= 0)
 		{
+			usleep(30);
 			if (c && ((c >> bit_count) & 1))
 				kill(pid, SIGUSR1);
 			else
 				kill(pid, SIGUSR2);
-			usleep(200);
+			usleep(30);
 		}
 		byte_count++;
 	}
-	ft_printf("Message successfully sent to server.");
 }
 
 int	main(int ac, char **av)
--- server.c
+++ server.c
@@ -6,17 +6,20 @@
 /*   By: mliew < mliew@student.42kl.edu.my>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/15 16:01:36 by mliew             #+#    #+#             */
-/*   Updated: 2022/08/05 12:34:37 by mliew            ###   ########.fr       */
+/*   Updated: 2022/08/05 16:06:32 by mliew            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static void	server_handler(int sig)
+static void	server_handler(int sig, siginfo_t *info, void *context)
 {
 	static int	c;
 	static int	bit_count;
+	static int	client_pid;
 
+	(void)context;
+	client_pid = info->si_pid;
 	if (sig == SIGUSR1)
 		c += 1;
 	if (++bit_count == 8)
@@ -30,8 +33,12 @@ static void	server_handler(int sig)
 
 int	main(void)
 {
-	signal(SIGUSR1, server_handler);
-	signal(SIGUSR2, server_handler);
+	struct sigaction	sa;
+	
+	sa.sa_sigaction = server_handler;
+	sa.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 	ft_printf("Server PID: %d\n", getpid());
 	while (1)
 		pause();
